64ce26fbc24e3457bd7452f0767cdef8f3db7d8b
EdnilsonSarmento <ednilsonsarmento9@gmail.com>
2020-03-13 11:37:01 -0300

    Second commit

--- src/main/java/controller/Controller.java
+++
@@ -1,7 +0,0 @@
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@Controller
-public class HomeController{
-
-}
---
+++ src/main/java/kriattus/lesproduct/HomeContoller.java
@@ -0,0 +1,15 @@
+package kriattus.lesproduct;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeContoller {
+    @GetMapping("/")
+    public ModelAndView index(){
+        ModelAndView model = new ModelAndView("index");
+        model.addObject("message", "Hello Yose");
+        return model;
+    }
+}
