6301803f4187252126daa71d81f221093f829729
amassiguile <amassinguile@unilurio.ac.mz>
2020-03-16 21:12:07 +0200

    power of two first try

--- src/main/java/kriattus/lesproduct/RestController.java
+++ src/main/java/kriattus/lesproduct/RestController.java
@@ -2,6 +2,7 @@ package kriattus.lesproduct;
 
 
 
+import net.minidev.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,5 +18,24 @@ public class RestController {
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("alive", true);
         return new ResponseEntity<Object>(map, HttpStatus.OK);
+    }
+    @GetMapping("/index")
+    public ResponseEntity<Object> powerOfTwo(int num){
+        String decomposition=null;
+        int aux=1;
+        decomposition = decomposition+"[";
+        for (int j = 1; j < num; j++) {
+            aux=aux*2;
+
+            if (aux<=num){
+                break;
+            }
+            decomposition = decomposition+"2";
+        }
+        decomposition = decomposition+"]";
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("number", num);
+        map.put("decomposition", decomposition);
+        return new ResponseEntity<Object>( map,HttpStatus.OK);
     }
 }
