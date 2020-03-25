4050a09c313e14c1eef96ea0baa57119abd38514
amassiguile <amassinguile@unilurio.ac.mz>
2020-03-25 10:23:42 +0200

    van

--- src/main/java/kriattus/lesproduct/RestController.java
+++ src/main/java/kriattus/lesproduct/RestController.java
@@ -21,59 +21,60 @@ public class RestController {
         return new ResponseEntity<Object>(map, HttpStatus.OK);
     }
     @GetMapping("/primeFactors")
-    public List<HashMap<String, Object>> primeFactors(String number){
+    public ResponseEntity<Object> primeFactors(String number){
        HashMap <String,Object> map = new HashMap<String,Object>();
-        HashMap <String,Object> map1 = new HashMap<String,Object>();
-        HashMap <String,Object> map2 = new HashMap<String,Object>();
-        List<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
-        String text[] = number.split(",");
-            map2.put("number",text[2]);
-            map2.put("error","not a number");
-            int num = Integer.parseInt(text[0]);
-            int num1 = Integer.parseInt(text[1]);
-            map1.put("number",num1);
-            map1.put("decomposition", primeFactores(num1));
-            map.put("number",num);
-            map.put("decomposition", primeFactores(num));
-        list.add(map);
-        list.add(map1);
-        list.add(map2);
+       if(number.contains(",")) {
+           HashMap<String, Object> map1 = new HashMap<String, Object>();
+           HashMap<String, Object> map2 = new HashMap<String, Object>();
+           List<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
+           String text[] = number.split(",");
+           map2.put("number", text[2]);
+           map2.put("error", "not a number");
+           int num = Integer.parseInt(text[0]);
+           int num1 = Integer.parseInt(text[1]);
+           map1.put("number", num1);
+           map1.put("decomposition", primeFactores(num1));
+           map.put("number", num);
+           map.put("decomposition", primeFactores(num));
+           list.add(map);
+           list.add(map1);
+           list.add(map2);
 
-        return list;
 
+           return new ResponseEntity<Object>(list, HttpStatus.OK);
+       }
+       else{
+
+           if (number.matches(".*[a-z].*")){
+               map.put("number",number);
+               map.put("error","not a number");
+               return new ResponseEntity<Object>(map, HttpStatus.OK);
+           }else {
+               int num = Integer.parseInt(number);
+               ArrayList<Integer> str = new ArrayList<Integer>();
+
+               Integer aux = 1;
+               for (int j = 1; j < num; j++) {
+                   aux = aux * 2;
+                   str.add(2);
+                   if (aux >= num) {
+                       break;
+                   }
+               }
+
+               map.put("number",number);
+               if (num>1000000){
+                   map.put("error","too big number (>1e6)");
+               }
+               else{
+                   map.put("decomposition", primeFactores(num));
+               }
+           }
+           return new ResponseEntity<Object>(map, HttpStatus.OK);
+       }
     }
 
-    public HashMap<String, Object> primeFactors(String []number){
-        HashMap<String, Object> map = new HashMap<String, Object>();
-        String text = number[0];
-        if (text.matches(".*[a-z].*")){
-            map.put("number",number);
-            map.put("error","not a number");
-            return map;
-        }else {
-            int num = Integer.parseInt(number[0]);
-            ArrayList<Integer> str = new ArrayList<Integer>();
 
-            Integer aux = 1;
-            for (int j = 1; j < num; j++) {
-                aux = aux * 2;
-                str.add(2);
-                if (aux >= num) {
-                    break;
-                }
-            }
-
-            map.put("number",number);
-            if (num>1000000){
-                map.put("error","too big number (>1e6)");
-            }
-            else{
-                map.put("decomposition", primeFactores(num));
-            }
-        }
-        return map;
-
-    }
     public static List<Integer> primeFactores(long number) {
         List<Integer> primefactors = new ArrayList<>();
      long copyOfInput = number;
